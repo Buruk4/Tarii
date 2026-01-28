00a706aa3d0fc5f8bc3de54100a029d73ab00644
Buruk4 <ifaoking32@gmail.com>
2026-01-28 07:46:20 +0300

    removed unneccessary

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
 // Load env vars
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 // Connect to database
 connectDB();
@@ -18,19 +18,21 @@ app.use(express.json());
 app.use(cors());
 
 // Mount routers
-app.use('/api/v1/auth', require('./routes/auth'));
-app.use('/api/v1/dailymarkings', require('./routes/dailyMarking'));
-app.use('/api/v1/trades', require('./routes/trades'));
+app.use("/api/v1/auth", require("./routes/auth"));
+app.use("/api/v1/dailymarkings", require("./routes/dailyMarking"));
+app.use("/api/v1/trades", require("./routes/trades"));
+app.use("/api/v1/stats", require("./routes/stats"));
+app.use("/api/v1/dashboard", require("./routes/dashboard"));
 
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
 );
 
 // Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
+process.on("unhandledRejection", (err, promise) => {
   console.log(`Error: ${err.message}`);
   // Close server & exit process
   server.close(() => process.exit(1));
